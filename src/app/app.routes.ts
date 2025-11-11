52f6fa6b9e088d94e2c95abce99d72781d21dadb
Andy Chiroque <andyalessandrochiroqueadanaque@gmail.com>
2025-11-11 16:29:48 -0500

    Implement routing for favorite page and update header links; enhance footer and favorite component structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
 import { Home } from './components/home/home';
+import { Favorite } from './components/favorite/favorite';
 
 export const routes: Routes = [
     {path: '', component: Home  },
+    {path: 'favorite', component: Favorite},
+    {path: 'home', component: Home},
 ];
